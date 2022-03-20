fix: Change average to 'macro'

src/model.py
@@ -66,8 +66,6 @@ def train_model():
     train = train.map(tokenize)
     val = val.map(tokenize)
 
-    breakpoint()
-
     # Initialise the model
     model = AutoModelForSequenceClassification.from_pretrained(
         model_id,
@@ -77,9 +75,9 @@ def train_model():
     )
 
     # Load the F1 metric and define the `compute_metrics` function
-    f1_metric = load_metric('f1')
-    precision_metric = load_metric('precision')
-    recall_metric = load_metric('recall')
+    f1_metric = load_metric('f1', average='macro')
+    precision_metric = load_metric('precision', average='macro')
+    recall_metric = load_metric('recall', average='macro')
     def compute_metrics(eval_pred):
         preds, labels = eval_pred
         preds = np.argmax(preds, axis=1)