fix: Set average='macro' during __call__

src/model.py
@@ -75,13 +75,13 @@ def train_model():
     )
 
     # Load the F1 metric and define the `compute_metrics` function
-    f1_metric = load_metric('f1', average='none')
-    precision_metric = load_metric('precision', average='macro')
-    recall_metric = load_metric('recall', average='macro')
+    f1_metric = load_metric('f1')
+    precision_metric = load_metric('precision')
+    recall_metric = load_metric('recall')
     def compute_metrics(eval_pred):
         preds, labels = eval_pred
         preds = np.argmax(preds, axis=1)
-        params = dict(predictions=preds, references=labels)
+        params = dict(predictions=preds, references=labels, average='macro')
         f1 = f1_metric.compute(**params)
         precision = precision_metric.compute(**params)
         recall = recall_metric.compute(**params)