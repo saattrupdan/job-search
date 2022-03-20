ab6c14ab45694c6b071bae74f0ff5b40eb48d5f1
saattrupdan <saattrupdan@gmail.com>
2022-03-20 19:47:54 +0000

    fix: Label dtype

--- src/model.py
+++ src/model.py
@@ -37,7 +37,7 @@ def train_model():
     # Convert the data to a HuggingFace dataset
     #labels = df[['title_or_tasks', 'requirements', 'bad']].values
     dataset = Dataset.from_dict(dict(text=df.cleaned_text.tolist(),
-                                     label=df.bad.tolist()))
+                                     label=df.bad.astype(float).tolist()))
 
     # Split the dataset into training and validation sets
     splits = dataset.train_test_split(train_size=0.8)
