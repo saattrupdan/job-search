ab6892def6554f00295ca1452a639bc0810d998a
saattrupdan <saattrupdan@gmail.com>
2022-03-22 11:15:56 +0000

    feat: Create EmailBot class

--- src/emailbot.py
+++ src/emailbot.py
@@ -1,20 +1,45 @@
 '''Automatic sending of emails with newest job listings'''
 
 import yagmail
+from typing import List
 
 
-def send_email(subject: str, content: str):
-    '''Send email with subject and content.
+class EmailBot:
+    '''Automatic sending of emails with newest job listings'''
 
-    Args:
-        subject (str):
-            Subject of email
-        content (str):
-            Content of email
-    '''
-    # receiver = 'amy.smart1@btinternet.com'
-    yag = yagmail.SMTP('amysjobbot')
-    yag.send(to='saattrupdan@gmail.com', subject=subject, contents=content)
+    def __init__(self):
+        self.email = yagmail.SMTP('amysjobbot')
+
+    def send_job_listings(self, job_listings: List[dict], to: str):
+        '''Send job listings an email.
+
+        Args:
+            job_listings (list of str):
+                List of job listings to send.
+            to (str):
+                Email address to send to.
+        '''
+        subject = '[YourFavoriteJobBot] New Job Listings'
+        content = ('Hiiii, it\'s me again!\n\n'
+                   'Here are the latest job listings:\n\n')
+        for idx, job_listing in enumerate(job_listings):
+            content += f'{idx}. {job_listing["url"]}\n\n'
+        content += '\n\nCheerio,\nYour Favorite Job Bot x'
+        self.send_email(subject=subject, content=content, to=to)
+
+    def send_email(self, subject: str, content: str, to: str):
+        '''Send email with subject and content.
+
+        Args:
+            subject (str):
+                Subject of email.
+            content (str):
+                Content of email.
+            to (str):
+                Email address to send to.
+        '''
+        # receiver = 'amy.smart1@btinternet.com'
+        self.email.send(to=to, subject=subject, contents=content)
 
 
 if __name__ == '__main__':
