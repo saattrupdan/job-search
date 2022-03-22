0d8489fa6b6261dd861931de786a52a97c7fc4d4
saattrupdan <saattrupdan@gmail.com>
2022-03-22 12:55:34 +0000

    fix: Don't drop url

--- src/model.py
+++ src/model.py
@@ -147,8 +147,7 @@ def train_relevance_model():
         extra_df = (df.sample(frac=0.5, replace=False, random_state=i)
                       .groupby('url')
                       .agg(dict(cleaned_text=lambda x: ' '.join(x),
-                                bad=lambda x: any(x)))
-                      .drop(columns='url'))
+                                bad=lambda x: any(x))))
         dfs.append(extra_df)
     df = pd.concat(dfs)
 
